13a6d9c38bfa2ad3ec7dcb632006ca2cf88cc698
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-03 06:48:47 +0300

    Update main module path in package.js
    Signed-off-by: Jørgen Vatle <Jorgen@WcAServices.net>

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2');
   api.use('ecmascript');
-  api.mainModule('dist/index.js');
+  api.mainModule('dist/CoinbaseCommerce.js');
 });
 
 Package.onTest(function(api) {
