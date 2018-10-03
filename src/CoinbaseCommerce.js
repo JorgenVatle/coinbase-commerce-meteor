2fa0cc11ce30b519ed243e968e33fd2b0acf10c7
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-03 03:12:00 +0300

    Add Coinbase Commerce class foundation.

---
+++ src/CoinbaseCommerce.js
@@ -0,0 +1,55 @@
+import { HTTP } from 'meteor/http';
+
+export default class CoinbaseCommerce {
+
+    /**
+     * Commerce API URL.
+     *
+     * @protected
+     * @type {string}
+     */
+    apiUrl = 'https://api.commerce.coinbase.com/';
+
+    /**
+     * Commerce API Key.
+     *
+     * @private
+     */
+    apiKey;
+
+    /**
+     * Coinbase Commerce constructor.
+     *
+     * @param apiKey
+     */
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    /**
+     * Builds a path to the API.
+     *
+     * @protected
+     * @param path
+     * @returns {string}
+     */
+    buildUrl(path) {
+        return this.apiUrl + path.replace(/^\/+/, '');
+    }
+
+    /**
+     * Send a request to the API.
+     *
+     * @param method
+     * @param path
+     * @param data
+     * @param options
+     */
+    request(method, path, data, options) {
+        HTTP.call(method, path, Object.assign({
+            url: this.apiUrl +
+            data,
+        }, options))
+    }
+
+}
