79800bb174e083c05e1387241a3249dcb7a7218d
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-03 03:56:46 +0300

    Add ChargeResource interface along with associated interfaces.

--- src/CoinbaseCommerceInterfaces.ts
+++ src/CoinbaseCommerceInterfaces.ts
@@ -3,10 +3,64 @@ export interface Money {
     currency: 'USD', // Todo: extend
 }
 
+interface TimelineEntry {
+    time: Timestamp,
+    status: 'NEW' | 'PENDING' | 'CONFIRMED' | 'UNRESOLVED' | 'RESOLVED',
+    context?: 'UNDERPAID',
+}
+
+interface Pricing {
+    local: Money,
+    bitcoin: Money,
+    ethereum: Money,
+    litecoin: Money,
+    bitcoin_cash: Money,
+    ethereum_classic: Money,
+}
+
+interface Addresses {
+    bitcoin: string,
+    ethereum: string,
+    litecoin: string,
+    bitcoin_cash: string,
+    ethereum_classic: string,
+}
+
+type Payments = Array<any>
+
+type PricingType = 'no_price' | 'fixed_price'
+
+type Timestamp = string;
+
+type Timeline = Array<TimelineEntry>
+
+export interface CheckoutResource {
+    id: string,
+}
+
+export interface ChargeResource {
+    id: string,
+    resource: 'charge',
+    code: string,
+    name: string,
+    description: string,
+    logo_url: string,
+    hosted_url: string,
+    created_at: Timestamp,
+    expires_at: Timestamp,
+    confirmed_at: Timestamp,
+    checkout: CheckoutResource,
+    timeline: Timeline,
+    metadata: any,
+    pricing_type: PricingType,
+    pricing: Pricing,
+    addresses: Addresses,
+}
+
 export interface CreateACharge {
     name: string,
     description: string,
-    pricing_type: 'no_price' | 'fixed_price',
+    pricing_type: PricingType,
     local_price?: Money;
     metadata?: any,
     redirect_url?: string,
