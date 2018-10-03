4e286df761077706586e750092f3fc7ece7a069a
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-03 07:20:20 +0300

    Assert that new charges can be created with CoinbaseCommerce.
    Signed-off-by: Jørgen Vatle <Jorgen@WcAServices.net>

--- tests/coinbase-commerce.test.js
+++ tests/coinbase-commerce.test.js
@@ -1,7 +1,22 @@
 import { Tinytest } from "meteor/tinytest";
 
-import { name as packageName } from "meteor/jorgenvatle:coinbase-commerce-meteor";
+import CoinbaseCommerce from "meteor/jorgenvatle:coinbase-commerce-meteor";
+const config = Meteor.settings.coinbase;
+const Commerce = new CoinbaseCommerce(config.key, config.secret);
 
-Tinytest.add('coinbase-commerce-meteor - example', function (test) {
-  test.equal(packageName, "coinbase-commerce-meteor");
+
+Tinytest.add('Can create new charges', function (test) {
+    const name = 'The Sovereign Individual';
+
+    const charge = Commerce.createCharge({
+      name,
+      description: 'Mastering the Transition to the Information Age',
+      pricing_type: 'fixed_price',
+      local_price: {
+          amount: '100.00',
+          currency: 'USD'
+      },
+    });
+
+    test.equal(charge.name, name);
 });
